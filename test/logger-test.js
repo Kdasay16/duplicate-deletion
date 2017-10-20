58c9d43abda06a5b428b8df1c153a59516e0b49d
Kayla Asay <kayla-asay@Ubuntu16.04-01>
2017-10-20 14:31:31 -0700

    test cases

---
+++ test/logger-test.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const expect = require('chai').expect
+
+describe('logger file', function() {
+  it('should log source code representation', () => {
+
+  })
+  it('should log the output record', () => {
+
+  })
+  it('should log individual changes', () => {
+
+  })
+})
